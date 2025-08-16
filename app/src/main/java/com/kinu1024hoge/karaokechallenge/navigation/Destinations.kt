2d10488788046d65befdd84a81c2ef5277e85a11
gear_kinu <techgit1024@gmail.com>
2025-08-16 23:57:48 +0900

    add: (#19)のChalleng->Result->Homeの画面遷移実装

--- app/src/main/java/com/kinu1024hoge/karaokechallenge/navigation/Destinations.kt
+++ app/src/main/java/com/kinu1024hoge/karaokechallenge/navigation/Destinations.kt
@@ -3,6 +3,8 @@ package com.kinu1024hoge.karaokechallenge.navigation
 object Destinations {
     const val HOME = "home"
     const val CHALLENGE = "challenge"
-    const val RESULT = "result"
+    const val RESULT = "result?score={score}"
     const val TESTPING = "testPing"
+
+    fun result(score: Int) = "result?score=$score"
 }
