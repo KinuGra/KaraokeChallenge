b96de8622f8b8188ff2877497fc5d0a70d94886b
gear_kinu <techgit1024@gmail.com>
2025-08-16 18:37:38 +0900

    remove: 無駄なimport削除

--- app/src/main/java/com/kinu1024hoge/karaokechallenge/feature/home/HomeScreen.kt
+++ app/src/main/java/com/kinu1024hoge/karaokechallenge/feature/home/HomeScreen.kt
@@ -8,20 +8,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-// ... 他のimport文 ...
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-// import androidx.compose.ui.res.painterResource // アイコンを使用しないためコメントアウトまたは削除
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-// import com.kinu1024hoge.karaokechallenge.R // アイコンを使用しないためコメントアウトまたは削除
 
 // UIの実際のコンテンツ部分
 @Composable
